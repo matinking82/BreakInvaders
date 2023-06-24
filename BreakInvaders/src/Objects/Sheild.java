6319340331a92c3625f98f8cb39def964011d16b
saqar-az <saqaraezzi@gmail.com>
2023-06-24 02:00:23 -0700

    complete sheild

--- BreakInvaders/src/Objects/Sheild.java
+++ BreakInvaders/src/Objects/Sheild.java
@@ -1,5 +1,57 @@
 package Objects;
 
-public class Sheild {
+import interfaces.IShowableObject;
+import processing.core.PApplet;
+import interfaces.ICollectibleItem;
+
+public class Sheild implements IShowableObject,ICollectibleItem{
+   PApplet processing;
+    private   int sheildwidth;
+    private   int sheildheight;
+    private   int sheildX;
+    private   int sheildY;
+    static boolean sheildAffect=false;
+
+
+   public Sheild(int sheildX,int setEllipseY,int sheildheight,int sheildwidth,PApplet procesisng){
+    
+    this.sheildheight=sheildheight;
+    this.sheildwidth=sheildwidth;
+    this.processing=processing;
+   }
+
+    @Override
+    public void show() {
+     //processing.image(processing.loadImage("../images/"), sheildX, sheildX, sheildX, sheildX, sheildwidth, sheildheight, sheildY, sheildX);
+    }
+
+    @Override
+    public void Collect() {
+
+    }
+    public int getShieldX(){
+        return this.sheildX;
+    }
+    public int getShieldY(){
+        return this.sheildY;
+    }
+    public int getShieldHeight(){
+        return this.sheildheight;
+    }
+    public int getShieldWidth(){
+        return this.sheildwidth;
+    }
+    public void setSheildX(int x){
+    this.sheildX=x;
+    }
+    public void setSheildY(int y){
+        this.sheildY=y;
+    }
+    public void setSheildHeight(int h){
+      this.sheildheight=h;
+      }
+    public void setShieldWidth(int w){
+       this.sheildwidth=w;
+         }
     
 }
