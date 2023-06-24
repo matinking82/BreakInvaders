a76f0b36015c2d2ed0419ea85dac2dfcb9d235a9
matinking82 <Matinking82@gmail.com>
2023-06-24 10:34:20 +0330

    add database

--- BreakInvaders/src/DbContext/interfaces/IDatabaseContext.java
+++ BreakInvaders/src/DbContext/interfaces/IDatabaseContext.java
@@ -1,5 +1,10 @@
 package DbContext.interfaces;
 
+import java.util.List;
+
+import DbContext.models.GameRecord;
+
 public interface IDatabaseContext {
-    
+    List<GameRecord> getTopGameRecords();
+    boolean AddGameRecord(GameRecord record);
 }
--- BreakInvaders/src/DbContext/services/DatabaseContext.java
+++ BreakInvaders/src/DbContext/services/DatabaseContext.java
@@ -1,7 +1,91 @@
 package DbContext.services;
 
-import DbContext.interfaces.IDatabaseContext;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class DatabaseContext implements IDatabaseContext{
-    
+import DbContext.interfaces.IDatabaseContext;
+import DbContext.models.GameRecord;
+
+public class DatabaseContext implements IDatabaseContext {
+
+    Connection connection;
+
+    public DatabaseContext() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:sqlite/db/breakInvaders.db";
+            // create a connection to the database
+            connection = DriverManager.getConnection(url);
+
+            String gameRecord = "CREATE TABLE IF NOT EXISTS GameRecord ("
+                    + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "date VARCHAR(50),"
+                    + "score INTEGER"
+                    + ");";
+
+            var sql = connection.createStatement();
+            sql.execute(gameRecord);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<GameRecord> getTopGameRecords() {
+        List<GameRecord> records = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM GameRecord ORDER BY score DESC LIMIT 10;";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String date = resultSet.getString("date");
+                int score = resultSet.getInt("score");
+                GameRecord record = new GameRecord();
+                record.setId(id);
+                record.setDate(date);
+                record.setScore(score);
+
+                records.add(record);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return records;
+    }
+
+    @Override
+    public boolean AddGameRecord(GameRecord record) {
+        try {
+            String maxIdSql = "SELECT MAX(id) FROM GameRecord;";
+            PreparedStatement maxIdStatement = connection.prepareStatement(maxIdSql);
+            ResultSet maxIdResult = maxIdStatement.executeQuery();
+            int maxId = maxIdResult.getInt(1);
+            record.setId(maxId + 1);
+
+            String sql = "INSERT INTO GameRecord(id, date, score) VALUES(?,?,?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, record.getId());
+            statement.setString(2, record.getDate());
+            statement.setInt(3, record.getScore());
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
 }
