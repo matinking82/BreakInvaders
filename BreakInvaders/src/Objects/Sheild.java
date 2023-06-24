9e9c172707562a50be453c20bcd45e42b925e500
matinking82 <Matinking82@gmail.com>
2023-06-24 13:58:45 +0330

    fix shield

--- BreakInvaders/src/Objects/Sheild.java
+++ BreakInvaders/src/Objects/Sheild.java
@@ -4,59 +4,67 @@ import interfaces.IShowableObject;
 import processing.core.PApplet;
 import interfaces.ICollectibleItem;
 
-public class Sheild implements IShowableObject,ICollectibleItem{
-   PApplet processing;
-    private   int sheildwidth;
-    private   int sheildheight;
-    private   int sheildX;
-    private   int sheildY;
-    static boolean sheildAffect=false;
+public class Sheild implements IShowableObject, ICollectibleItem {
+    PApplet processing;
+    private int sheildwidth;
+    private int sheildheight;
+    private int sheildX;
+    private int sheildY;
+    static boolean sheildAffect = false;
 
+    public Sheild(int sheildheight, int sheildwidth, PApplet processing) {
 
-   public Sheild(int sheildX,int setEllipseY,int sheildheight,int sheildwidth,PApplet procesisng){
-    
-    this.sheildheight=sheildheight;
-    this.sheildwidth=sheildwidth;
-    this.processing=processing;
-   }
+        this.sheildheight = sheildheight;
+        this.sheildwidth = sheildwidth;
+        this.processing = processing;
+    }
 
     @Override
     public void show() {
-     //processing.image(processing.loadImage("../images/"), sheildX, sheildX, sheildX, sheildX, sheildwidth, sheildheight, sheildY, sheildX);
+        processing.image(processing.loadImage("../images/Shield.png"), sheildX, sheildY, sheildwidth,
+                sheildheight);
     }
 
     @Override
     public void Collect() {
 
     }
-    public int getShieldX(){
+
+    public int getShieldX() {
         return this.sheildX;
     }
-    public int getY(){
+
+    public int getY() {
         return this.sheildY;
     }
-    public int getShieldHeight(){
+
+    public int getShieldHeight() {
         return this.sheildheight;
     }
-    public int getShieldWidth(){
+
+    public int getShieldWidth() {
         return this.sheildwidth;
     }
-    public void setSheildX(int x){
-    this.sheildX=x;
+
+    public void setSheildX(int x) {
+        this.sheildX = x;
     }
-    public void setSheildY(int y){
-        this.sheildY=y;
+
+    public void setSheildY(int y) {
+        this.sheildY = y;
     }
-    public void setSheildHeight(int h){
-      this.sheildheight=h;
-      }
-    public void setShieldWidth(int w){
-       this.sheildwidth=w;
-         }
+
+    public void setSheildHeight(int h) {
+        this.sheildheight = h;
+    }
+
+    public void setShieldWidth(int w) {
+        this.sheildwidth = w;
+    }
 
     @Override
     public void move() {
 
     }
-    
+
 }
