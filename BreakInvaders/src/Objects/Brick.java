5d529371ebcf9238426e99cd1802362ce016c1e9
kosar <salarikosar5@gmail.com>
2023-06-24 11:11:52 +0330

    complete Brick class

--- BreakInvaders/src/Objects/Brick.java
+++ BreakInvaders/src/Objects/Brick.java
@@ -13,13 +13,32 @@ public class Brick implements IShowableObject ,IHittableObject  {
     // public static int speedY=-80;
     private int Blocky;
     private int Blockx;
+    private int heart;
 
 
-    Public Brick(int width  , int height  , int level , PApplet processing)
+    public Brick(int width  , int height  , int level , PApplet processing)
     {
         this.width=width;
         this.height=height;
         this.level=level;
+        switch (level) {
+            case 1:
+              heart=2;
+              break;
+            case 2:
+                heart=4; 
+                break;
+
+            case 3:
+                heart=6;
+                break; 
+            case 4:
+               heart=10;
+               break;
+        
+            default:
+                break;
+        }
         this.processing=processing;
 
 
@@ -57,12 +76,13 @@ public class Brick implements IShowableObject ,IHittableObject  {
     }
     public void show()
     {
-        
+        processing.image(processing.loadImage("../images/chick"+level+".png"), Blockx, Blocky, width, height);
+
 
     }
     public void hit()
     {
-        level--;
+        heart--;
 
     }
     
